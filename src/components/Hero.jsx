0bf882c6cdc4c4066ff3f3ce830477f7d4a61ece
Glen <0xJozeth@gmail.com>
2023-08-07 17:05:41 -0400

    update favicon, logo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import { logo } from '../assets';
 
 const Hero = () => {
-	return <div>Hero</div>;
+	return (
+		<header className='w-full flex justify-center items-center flex-col'>
+			<nav className='flex justify-between items-center w-full mb-10'>
+				<img
+					src={logo}
+					alt='summarly_logo'
+					className='w-28 m-8 object-contain'
+				/>
+
+				<button
+					type='button'
+					onClick={() => window.open('htttps://github.com/0xJozeth')}
+					className='black_btn'
+				>
+					Github
+				</button>
+			</nav>
+		</header>
+	);
 };
 
 export default Hero;
